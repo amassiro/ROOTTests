64593d49c36010991335bdca004b77ec11144cc4
amassiro <massironi.andrea@gmail.com>
2019-04-24 17:10:38 +0200

    new rdf

---
+++ RDataFrame/RDataFrameFilter.cxx
@@ -0,0 +1,55 @@
+{
+  
+  //
+  // Ad hoc tree generation
+  //
+  
+  TTree* tree = new TTree("tree", "tree");
+  float a_v[100];
+  int b;
+  tree->Branch("a_v",a_v, "a_v[100]/F");
+  tree->Branch("b",&b, "b/I");
+  
+  for (int i=0; i<1000; i++){
+    for (int j=0; j<100; j++){
+      a_v[j] =  j+i;
+      b = i%10;
+    }
+    tree->Fill();
+  }
+  
+  tree->Print();
+  
+  
+  ROOT::RDataFrame dataframe_data (*tree);
+  
+  
+  TCanvas* cc = new TCanvas ("cc", "only b", 800, 600);
+  auto histo = dataframe_data.Histo1D("b");
+  histo->Draw();
+  
+  
+  TCanvas* cc_simple = new TCanvas ("cc_simple", "simple", 800, 600);
+  auto count_simple = dataframe_data.Filter ("b>3").Count();
+  std::cout << "count_simple = " << *count_simple << std::endl;
+  
+  auto simple = dataframe_data.Filter ("b>3");
+  std::cout << "count  = " << *(simple.Count()) << std::endl;
+  
+  auto histo_simple = simple.Histo1D("b");
+  histo_simple->Draw();
+  
+
+  
+  
+  
+  TCanvas* cc_simple_vect = new TCanvas ("cc_simple_vect", "vect", 800, 600);
+  
+  auto df_vect = dataframe_data.Filter ("a_v[b]>3");
+  std::cout << "count  = " << *(df_vect.Count()) << std::endl;
+  
+  
+  
+  
+  
+}
---
+++ RDataFrame/README.md
@@ -0,0 +1,5 @@
+Test RDataFrame
+====
+
+    access to vector and position
+
